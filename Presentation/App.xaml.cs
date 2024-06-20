e4d1e384ae302f4263613a6e4153afce580d6195
TelurXS <vacaroslav@gmail.com>
2024-06-20 17:06:45 +0300

    Project Init

---
+++ Presentation/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+
+namespace Presentation;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    public App()
+    {
+        Services.AddSingleton(this);
+        Services.AddTransient<MainWindow>();
+
+        Provider = Services.BuildServiceProvider();
+    }
+
+    private ServiceCollection Services { get; } = new();
+    private ServiceProvider Provider { get; }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        var window = Provider.GetRequiredService<MainWindow>();
+        window.Show();
+    }
+}
